7590be72a51a64dbb6d2cdb4096cc8512c2aceff
Saur03 <117138035+Saur03@users.noreply.github.com>
2024-09-27 21:36:39 -0400

    Add files via upload

---
+++ blog_oop/includes/config.php
@@ -0,0 +1,14 @@
+<?php
+date_default_timezone_set("America/Toronto");
+define("DB_HOST", "localhost");
+define("DB_USER", "schawla");
+define("DB_PASSWORD", "j2jpbj2jpb");
+define("DB_NAME", "schawlablog");
+
+//Add your blog name below
+$config_blogname = "The best blog in the Universe";
+
+// Add your name below
+$config_author = "Saurabh Chawla";
+
+?>
