1acd9ebd2d576d6dd64bb6f49d0827d037c3056d
Saurabh Chawla <W0838092@myscc.ca>
2024-10-05 23:59:00 -0400

    changes

---
+++ BLOG/session.php
@@ -0,0 +1,38 @@
+<?php
+
+class Session{
+    private $user;
+
+    public function __construct(){
+        session_start();
+        if(isset($_SESSION["user"])){
+            $this->user = $_SESSION["user"];
+        }
+    }
+
+    public function isLoggedIn(){
+        if($this->user) {
+            return $this->user;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    public function login($userObj) {
+        $this->user = $userObj;
+        $_SESSION['user'] = $userObj;
+    }
+
+   
+
+
+
+}
